1f84cf6bc4010852f2e6a75ee6596f0f7bdaa729
Talal Habib <talalhabib101@gmail.com>
2023-12-09 16:13:19 +0500

    Co-authored-by: Umar.exe <yuri8822@users.noreply.github.com>

--- Main.cpp
+++ Main.cpp
@@ -6,77 +6,137 @@
 
 using namespace std;
 
-const int INITIAL_BRICKS = 50;
-const int INITIAL_CEMENT = 30;
-const int INITIAL_TOOLS = 10;
+struct Resources
+{
+    sem_t Bricks; // One Unit means 50 bricks
+    sem_t Cement; // One Unit means 50 bags of cement
+    sem_t Tools;  // One Unit means 20 tools
 
-vector<vector<string>> tasks = {
-    {"Urgent repairs", "foundation laying", "critical structural work"},
-    {"General construction tasks", "bricklaying", "cement mixing"},
-    {"Non-critical tasks", "finishing touches", "aesthetic elements"}
+    Resources()
+    {
+        sem_init(&Bricks, 0, 20);
+        sem_init(&Cement, 0, 8);
+        sem_init(&Tools, 0, 5);
+    }
+} resources;
+
+struct Resource_Utilization
+{
+    int Bricks=0;
+    int Cement=0;
+    int History_Bricks=0;
+    int History_Cement=0;
+} resource_utilization;
+
+enum weather
+{
+    SUNNY,
+    NATURAL_DISASTER,
+    RAINY
 };
 
-sem_t bricksSemaphore, cementSemaphore, toolsSemaphore;
+enum Skillset
+{
+    ELECTRICIAN,
+    PLUMBER,
+    CARPENTER,
+    MASON,
+    LABORER,
+    ENGINEER
+};
+
+struct Task_Type
+{
+    int priority; // 1 = high, 2 = medium, 3 = low
+    string description;
+    int skillset;
+};
+
+vector<Task_Type> task_types = {
+    {3, "Lighting Installation", ELECTRICIAN},
+    {3, "Wiring for New Construction", ELECTRICIAN},
+    {3, "Stone Wall Construction", MASON},
+    {3, "Construction Project Management", ENGINEER},
+    {3, "Project Planning and Design", ENGINEER},
+
+    {2, "Cement Mixing", LABORER},
+    {2, "Brick Laying", MASON},
+    {2, "Sewer Line Installation", PLUMBER},
+    {2, "Gas Line Installation", PLUMBER},
+    {2, "Paving Pathways", MASON},
+
+    {1, "Pipe Installation and Repair", PLUMBER},
+    {1, "Door and Window Installation", CARPENTER},
+    {1, "Staircase Design and Installation", CARPENTER},
+    {1, "Troubleshooting Electrical Issues", ELECTRICIAN},
+    {1, "Concrete Repair", MASON}
+};
 
 pthread_mutex_t dataMutex = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct Worker
+{
+    int id;
+    vector<int> skills;
+    int SkillLevel;
+    bool onBreak; // Flag indicating if the worker is on break
+};
+
 struct Task
 {
     string name;
     int priority;
     string TaskDescription;
-    int AssignedWorker;
+    Worker *AssignedWorker;
 };
-struct Worker
-{
-    int id;
-    Task task;
-    int SkillLevel;
-    bool onBreak; // Flag indicating if the worker is on break
-};
-queue<Task> highPriorityQueue, mediumPriorityQueue, lowPriorityQueue;
 
-queue<Worker> workersQueue;
+queue<Task> highPriorityQueue, mediumPriorityQueue, lowPriorityQueue, OnHoldQueue;
+
+// On break means cannot work the rest of the day
+vector<Worker> OccupiedWorkers, OnBreakWorkers;
+queue<Worker> AvailableWorkers;
 
 int main()
 {
-    int numWorkers;
-    cout << "Enter the number of workers: ";
-    cin >> numWorkers;
-    cout << endl;
-
-    // Initialize semaphores
-    sem_init(&bricksSemaphore, 0, INITIAL_BRICKS);
-    sem_init(&cementSemaphore, 0, INITIAL_CEMENT);
-    sem_init(&toolsSemaphore, 0, INITIAL_TOOLS);
+    int numWorkers=rand()%21+10;
 
     // Initialize workers
     for (int i = 0; i < numWorkers; i++)
     {
-        Worker worker;
-        worker.id = i;
-        worker.SkillLevel = rand() % 3 + 1;
-        worker.onBreak = false;
-        workersQueue.push(worker);
+        Worker newWorker;
+        newWorker.id = i;
+        for (int i = 0; i < rand() % 4 + 1; i++)
+        {
+            newWorker.skills.push_back(rand() % 6);
+        }
+        newWorker.SkillLevel = rand() % 3 + 1;
+        newWorker.onBreak = false;
+        AvailableWorkers.push(newWorker);
     }
 
     // Initialize tasks
-    int numTasks = rand() % 10 + 1;
-    for (int i=0; i < numTasks; i++)
-    {
-        Task task;
-        task.name = "Task " + to_string(i);
-        task.priority = rand() % 3 + 1;
-        task.TaskDescription = tasks[task.priority - 1][rand() % 3];
-        task.AssignedWorker = -1;
-        pthread_mutex_lock(&queueMutex);
-        if (task.priority == 1)
-            highPriorityQueue.push(task);
-        else if (task.priority == 2)
-            mediumPriorityQueue.push(task);
-        else
-            lowPriorityQueue.push(task);
-        pthread_mutex_unlock(&queueMutex);
+    
+    while(true){
+        
     }
+
+    // int numTasks = rand() % 10 + 1;
+    // for (int i = 0; i < numTasks; i++)
+    // {
+    //     Task task;
+    //     task.name = "Task " + to_string(i);
+    //     task.priority = rand() % 3 + 1;
+    //     //task.TaskDescription = tasks[task.priority - 1][rand() % 3];
+    //     task.AssignedWorker = nullptr;
+    //     pthread_mutex_lock(&queueMutex);
+    //     if (task.priority == 1)
+    //         highPriorityQueue.push(task);
+    //     else if (task.priority == 2)
+    //         mediumPriorityQueue.push(task);
+    //     else
+    //         lowPriorityQueue.push(task);
+    //     pthread_mutex_unlock(&queueMutex);
+    // }
 }
